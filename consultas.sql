2a70e02e65851038511679ed80e6a39c77f359cd
Joao Cecilio <joaop.medeiros@gmail.com>
2019-05-03 14:54:41 -0300

    Consulta 2

--- consultas.sql
+++ consultas.sql
@@ -4,4 +4,11 @@
         inner join DUNCANBDA.MUNICIPIOS m on a.MUNIC_EMPREGADOR = m.COD_MUNIC
         inner join DUNCANBDA.MUNICIPIOS_POPULACOES p on m.COD_MUNIC = p.cod_munic
         group by m.NOME_MUNICÍPIO, m.NOME_UF, p.população, to_char(a.data_acidente,'MM-YYYY')
-        
+        order by count(1)/(p.população/100000) desc
+-- CONSULTA 2        
+    select a.AGENTE_CAUSADOR_ACIDENTE, to_char(a.data_acidente,'MM-YYYY') as mes_ano,  count(1) as acidentes
+        from DUNCANBDA.ACID_TRAB_2018JUL_2019MAR_11 a 
+        WHERE a.INDICA_OBITO_ACIDENTE = 'Sim'
+        group by a.AGENTE_CAUSADOR_ACIDENTE, to_char(a.data_acidente,'MM-YYYY')
+        having count(1) > 2
+     
